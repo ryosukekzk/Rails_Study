fc4a92875f803ce6de85437977b8ba196aa6779f
Ryosuke Kozuka <ryosuke@Ryosuke-no-MacBook-Air.local>
2012-09-12 17:06:20 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = '53c1924902b79b0bf9d180ecf8769c15b8fd4ceca4c14f630c8a9411baef078d30e0efb8b17a5c40aa89af5290ae2f9b75a2abcb514ebd24d4bbdb80375ecd86'
